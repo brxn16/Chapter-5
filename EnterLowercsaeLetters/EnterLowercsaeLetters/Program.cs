789c2c33e4d5e3b138739e2daec45c41d7e60133
Brxn16 <camplese123@gmail.com>
2018-12-03 13:10:32 -0600

    update

---
+++ EnterLowercsaeLetters/EnterLowercsaeLetters/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace EnterLowercsaeLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char letter;
+            string letterString;
+
+            WriteLine("Please enter a lowercsae number.");
+            letterString = ReadLine();
+            letter = Convert.ToChar(letterString);
+
+            if(letter >= 'a' && letter <= 'z')
+            {
+                WriteLine("OK");
+            }
+            else
+            {
+                WriteLine("Please make it a lowercase letter bozo!");
+            }
+        }
+    }
+}
---
+++ SumInts/SumInts/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace SumInts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num=0;
+            int sum = 0;
+            string inputString;
+           
+
+            while (num != 999)
+            {
+                WriteLine("Enter a number. When you wish to exit the program, type 999.");
+                inputString = ReadLine();
+                num = Convert.ToInt32(inputString);
+                sum = sum + num;
+
+            }
+            Write("The sum is " + (sum -999));
+
+        }
+    }
+}
