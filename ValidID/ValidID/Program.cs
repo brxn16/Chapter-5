a6552afa42d010e9d464795c8a88671ceb1d1a22
Brxn16 <camplese123@gmail.com>
2018-12-03 11:53:04 -0600

    update

---
+++ ValidID/ValidID/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace ValidID
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idNum;
+            string input;
+            const int LOW = 1000;
+            const int HIGH = 9999;
+
+            Write("Enter an ID Number");
+            input = ReadLine();
+            idNum = Convert.ToInt32(input);
+
+            while (idNum < LOW || idNum > HIGH)
+            {
+                WriteLine(idNum + " is an invalid ID number");
+                Write("ID numbers must be between" + LOW + " and " + HIGH + " inclusive");
+                Write("Enter an ID NUmber");
+                input = ReadLine();
+                idNum = Convert.ToInt32(input);
+            }
+            WriteLine("ID number " + idNum + " is valid.");
+        }
+    }
+}
