3355f4c88ec47084b254de4bc1a11660225bc261
Brxn16 <camplese123@gmail.com>
2018-12-06 11:56:28 -0600

    update

---
+++ TestScores/TestScores/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace TestScores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int test = 0;
+            const int MAX = 100;
+            const int MIN = 0;
+            string testString;
+            int average = 0;
+            int total = 0;
+            int count = 0;
+            int Invalid = 0;
+
+            while (test != 999)
+            { 
+                WriteLine("Please enter your test scores. Type 999 to show your average score.");
+                testString = ReadLine();
+                test = Convert.ToInt32(testString);
+                count++;
+                total = test + total;
+                if (test < MAX && test > MIN)
+                {
+                    WriteLine("Valid Score.");
+                }
+                else
+                {
+                    WriteLine("Invalid Score.");
+                    Invalid++;
+                }
+                count = count - Invalid;
+                average = total / count;
+
+            }
+            WriteLine("The average is " + average);
+
+        }
+    }
+}
